3f304ede1cdec6d3389066a078a1eda267bfc56a
jatin foujdar <jatin321001@gmail.com>
2025-02-09 23:29:47 +0530

    network manager commit

--- Movie-Fetch-IOS/Model/NetrorkLayer.swift
+++ Movie-Fetch-IOS/Model/NetrorkLayer.swift
@@ -1,8 +1,14 @@
-//
-//  NetrorkLayer.swift
-//  Movie-Fetch-IOS
-//
-//  Created by jatin foujdar on 09/02/25.
-//
-
 import Foundation
+
+class NetworkManager{
+    static let shared = NetworkManager()
+    
+    private init(){}
+    
+    func fetchMovies(page: Int = 1) async throws-> [Movie]{
+        let url = URL(string: "\(Constant.baseURL)/movie/popular?api_key=\(Constant.apikey)&page=\(page)")!
+        let(data,_) = try await URLSession.shared.data(from: url)
+        let movieResponse = try JSONDecoder().decode(MovieResponce.self, from: data)
+        return movieResponse.result
+    }
+}
