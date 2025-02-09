0db4769219a873d7b0f7285357df2d338d5b5d63
jatin foujdar <jatin321001@gmail.com>
2025-02-09 11:10:55 +0530

    Initial Commit

---
+++ Movie-Fetch-IOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Movie-Fetch-IOS
+//
+//  Created by jatin foujdar on 09/02/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Movie-Fetch-IOS/Movie_Fetch_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Movie_Fetch_IOSApp.swift
+//  Movie-Fetch-IOS
+//
+//  Created by jatin foujdar on 09/02/25.
+//
+
+import SwiftUI
+
+@main
+struct Movie_Fetch_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
